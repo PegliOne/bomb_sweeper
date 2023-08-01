c3622d72c734aef3e3028f201bfa8d0458de79f1
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2023-08-01 17:25:45 +1000

    Moved bomb creation logic to board

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -1,12 +1,31 @@
-import BoardRow from "./BoardRow";
+import { useEffect, useState } from "react";
+
+function createBombArray() {
+  return Array.from({length: 9}, () => Math.random() < 0.2 ? 'B' : '');
+}
 
 const Board = () => {
+  const [ bombMatrix, setBombMatrix ] = useState([]);
+
+  useEffect(() => {
+    const bombMatrix = Array.from({length: 9}, () => createBombArray());
+    setBombMatrix(bombMatrix);
+  }, [])
+
   if (window.location.pathname === '/') {
     return (
       <section class="board">
-        { Array.from({ length: 9 }, (_, i) =>
-          <BoardRow/>
-        )}
+        { bombMatrix.map((row) => {
+          return (
+            <div>
+              { row.map((square) => {
+                return (
+                  <div>{square}</div>
+                )  
+              })}
+            </div>
+          )
+        })}
       </section>
     );
   }
--- frontend/src/components/BoardSquare.jsx
+++
@@ -1,7 +0,0 @@
-const BoardSquare = ({ hasBomb }) => {
-  return (
-    <div>{ hasBomb ? 'B' : '' }</div>
-  );
-}
- 
-export default BoardSquare;
