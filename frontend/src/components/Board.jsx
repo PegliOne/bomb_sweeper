aecf26fca9637ae6a91db284f27c37f2ef9ec92f
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2023-08-07 15:45:14 +1000

    Fixed stylesheets, added harder boards and difficulty selection stylings

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -1,18 +1,34 @@
 import { useEffect, useState } from "react";
 
+// Simplify difficulty and board size selection logic
+
+const boardSizes = {
+  '/': [9, 9],
+  '/game/easy': [9, 9],
+  '/game/medium': [16, 16],
+  '/game/hard': [30, 16]
+}
+
+function getBoardSize(path) {
+  return boardSizes[path];
+}
+
 function createBombArray() {
-  return Array.from({length: 9}, () => Math.random() < 0.2 ? 'B' : '');
+  return Array.from({ length: getBoardSize(window.location.pathname.toLocaleLowerCase())[0] }, () => Math.random() < 0.2 ? 'B' : '');
 }
 
 const Board = () => {
   const [ bombMatrix, setBombMatrix ] = useState([]);
 
   useEffect(() => {
-    const bombMatrix = Array.from({length: 9}, () => createBombArray());
+    if (!['/', '/game/easy', '/game/medium', '/game/hard'].includes(window.location.pathname.toLowerCase())) {
+      return;
+    }
+    const bombMatrix = Array.from({ length: getBoardSize(window.location.pathname.toLocaleLowerCase())[1] }, () => createBombArray());
     setBombMatrix(bombMatrix);
   }, [])
 
-  if (['/game/easy', '/game/medium', '/game/hard'].includes(window.location.pathname.toLowerCase())) {
+  if (['/', '/game/easy', '/game/medium', '/game/hard'].includes(window.location.pathname.toLowerCase())) {
     return (
       <section className="board">
         { bombMatrix.map((row, index) => {
@@ -20,7 +36,7 @@ const Board = () => {
             <div key={index}>
               { row.map((square, index) => {
                 return (
-                  <div key={index}>{square} {index}</div>
+                  <div key={index}>{square}</div>
                 )  
               })}
             </div>
@@ -30,5 +46,5 @@ const Board = () => {
     );
   }
 }
- 
+
 export default Board;
