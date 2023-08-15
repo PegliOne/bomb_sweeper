33d420e87166f36c96dda76ac0c9a398dc96153b
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2023-08-15 12:40:18 +1000

    Replaced single quotes with double quotes and added bomb count logic

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -1,21 +1,25 @@
 import { useEffect, useState } from "react";
 
 const path = window.location.pathname;
-const difficulty = path === '/' ? 'easy' : path.slice(6).toLowerCase();
-const isValidGamePage = ['easy', 'medium', 'hard'].includes(difficulty);
+const difficulty = path === "/" ? "easy" : path.slice(6).toLowerCase();
+const isValidGamePage = ["easy", "medium", "hard"].includes(difficulty);
 const bombProbability = 0.125;
 
 const boardSizes = {
-  'easy': [9, 9],
-  'medium': [16, 16],
-  'hard': [30, 16]
+  "easy": [9, 9],
+  "medium": [16, 16],
+  "hard": [30, 16]
+}
+
+function checkValidLocation(location) {
+  return location.xCor > -1 && location.xCor < boardSizes[difficulty][0] && location.yCor > -1 && location.yCor < boardSizes[difficulty][1];
 }
 
 function createBombArray() {
   return Array.from({ length: boardSizes[difficulty][0] }, () => Math.random() < bombProbability ? { hasBomb: true } : { hasBomb: false });
 }
 
-function checkIfNextToBomb(bombMatrix, horIndex, verIndex) {
+function countBombs(bombMatrix, horIndex, verIndex) {
   const testLocations = [
     {
       xCor: horIndex - 1,
@@ -50,13 +54,11 @@ function checkIfNextToBomb(bombMatrix, horIndex, verIndex) {
       yCor: verIndex + 1
     },
   ];
-  const validTestLocations = testLocations.filter((location) => location.xCor > -1 && location.xCor < boardSizes[difficulty][0] && location.yCor > -1 && location.yCor < boardSizes[difficulty][1]);
-  const nextToBomb = !validTestLocations.every((location) => !bombMatrix[location.yCor][location.xCor].hasBomb);
-  return nextToBomb;
-}
 
-function renderSquare(square) {
-  return square.hasBomb ? 'B' : '';
+  const validTestLocations = testLocations.filter((location) => checkValidLocation(location));
+  const bombCount = validTestLocations.filter((location) => bombMatrix[location.yCor][location.xCor].hasBomb).length;
+
+  return bombCount;
 }
 
 const Board = () => {
@@ -72,13 +74,13 @@ const Board = () => {
 
   if (isValidGamePage) {
     return (
-      <section className={`board ${ difficulty === 'hard' ? 'large' : ''}`}>
+      <section className={`board ${ difficulty === "hard" ? "large" : ""}`}>
         { bombMatrix.map((row, verIndex) => {
           return (
             <div key={verIndex}>
               { row.map((square, horIndex) => {
                 return (
-                  <div key={horIndex}>{ square.hasBomb ? 'B' : checkIfNextToBomb(bombMatrix, horIndex, verIndex) ? 'N' : '' }</div>
+                  <div key={horIndex}>{ square.hasBomb ? "B" : countBombs(bombMatrix, horIndex, verIndex) === 0 ? "" : countBombs(bombMatrix, horIndex, verIndex) }</div>
                 )  
               })}
             </div>
