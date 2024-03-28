8c5d47f455d55c69c459f514ccb6f0d28bee0e4a
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2024-03-28 15:19:50 +1100

    Adds login form error message functionality, adds error message paragraph styling and updates error messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,14 @@ class SessionsController < ApplicationController
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path
-    end  
+    else
+      flash[:error] = "Error: Email or Password Incorrect"
+      redirect_to "/log_in" 
+    end    
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to "/"
-  end  
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,10 @@ class UsersController < ApplicationController
   def create
     user_emails = User.all.map{|user| user.email}
     if user_emails.include?(user_params[:email])
-      flash[:error] = "Error: Email Already In Use"
+      flash[:error] = "Error: Cannot Use Email That Is Already In Use"
       redirect_to "/sign_up"
     elsif user_params[:password] != user_params[:password_confirmation]
-      flash[:error] = "Error: Password And Password Confirmation Do Not Match"
+      flash[:error] = "Error: Password And Password Confirmation Must Match"
       redirect_to "/sign_up"  
     else
       @user = User.create(user_params)
