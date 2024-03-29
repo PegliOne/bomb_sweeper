04b41188423c3d4ed95507e4af822f7f101face6
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2024-03-29 13:10:52 +1100

    Creates separate methods for form validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,23 +8,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    usernames = User.all.map{|user| user.username}
-    emails = User.all.map{|user| user.email}
-    if usernames.include?(user_params[:username])
-      flash[:error] = "Error: Username Must Be Unique" 
-      redirect_to "/sign_up" 
-    elsif emails.include?(user_params[:email])
-      flash[:error] = "Error: Email Must Be Unique"
-      redirect_to "/sign_up"
-    elsif user_params[:password] != user_params[:password_confirmation]
-      flash[:error] = "Error: Password Confirmation Must Match Password"
-      redirect_to "/sign_up"  
-    else
-      @user = User.create(user_params)
-      if @user.save
-        session[:user_id] = @user.id
-        redirect_to root_path
-      end
+    return unless is_user_valid?
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
     end  
   end
 
@@ -44,6 +32,27 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
+
+  def is_user_valid?
+    usernames = User.all.map{|user| user.username}
+    emails = User.all.map{|user| user.email}
+    if usernames.include?(user_params[:username])
+      show_error "Error: Username Must Be Unique" 
+      return false
+    elsif emails.include?(user_params[:email])
+      show_error "Error: Email Must Be Unique"
+      return false
+    elsif user_params[:password] != user_params[:password_confirmation]
+      show_error "Error: Password Confirmation Must Match Password" 
+      return false
+    end    
+    return true
+  end
+
+  def show_error error
+    flash[:error] = error 
+    redirect_to "/sign_up"
+  end 
 
   def user
     User.find(@current_user.id)
