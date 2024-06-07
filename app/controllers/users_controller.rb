61939a0e51ed2debecc6246e274bb1fc2014057d
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2024-06-07 12:23:52 +1000

    Fix quickest time difficulty capitalisation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,9 +21,9 @@ class UsersController < ApplicationController
     @win_count = winning_plays.count
     unless @win_count == 0
       @win_percentage = ((@win_count / play_count) * 100).round(2)
-    end  
+    end
     @quickest_winning_play_time = quickest_winning_play&.time_in_seconds
-    @quickest_winning_play_difficulty = quickest_winning_play&.difficulty
+    @quickest_winning_play_difficulty = quickest_winning_play&.difficulty.capitalize
     @easy_plays = filter_plays_by_difficulty(winning_plays, "easy")
     @medium_plays = filter_plays_by_difficulty(winning_plays, "medium")
     @hard_plays = filter_plays_by_difficulty(winning_plays, "hard")
